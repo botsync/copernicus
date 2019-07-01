44da84bbeb65002a1423f15f2e1fa27005bbeee5
Nikhil Venkatesh <nikhil@botsync.sg>
2019-07-01 19:43:47 +0530

    Updated Copernicus Base for ROS Control Integration

---
+++ copernicus_base/include/copernicus_base/constants.h
@@ -0,0 +1,10 @@
+#define PI                          3.14159
+
+#define MAX_COMM_RETRY                  2
+
+#define MAX_RPM                     3000
+#define MIN_RPM                     100
+#define LINEAR_SPEED_LIMIT          1.0
+#define ANGULAR_SPEED_LIMIT         1.0
+#define MOTOR_REDUCTION             40
+#define WHEEL_DIAMETER              0.36
